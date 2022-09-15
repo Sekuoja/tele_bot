4c6bf80f5b274458cc15f36fad677f6c2154b680
sekvoja <krokus.xx@yande.xru>
2022-09-15 22:22:48 +0300

    version 1.0

--- main.py
+++ main.py
@@ -10,29 +10,46 @@ with open("token.txt", "r") as f:
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
-@dp.message_handler(commands=['расписание'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(msg: types.Message):
+    await msg.answer("""Все команды: 
+/help 
+/schedule 
+/today 
+/tomorrow 
+/next_week""")
+
+@dp.message_handler(commands=['schedule'])
+async def send_schedule(msg: types.Message):
     dashboard = schedule.get_schedule()
-    await msg.reply(dashboard, parse_mode="HTML")
+    await msg.answer(dashboard, parse_mode="HTML")
 
 
-@dp.message_handler(commands=['сегодня'])
-async def send_welcome(msg: types.Message):
+@dp.message_handler(commands=['today'])
+async def send_today(msg: types.Message):
     dashboard = schedule.get_schedule_today()
-    await msg.reply(dashboard, parse_mode="HTML")
+    await msg.answer(dashboard, parse_mode="HTML")
 
 
-@dp.message_handler(commands=['завтра'])
-async def send_welcome(msg: types.Message):
+@dp.message_handler(commands=['tomorrow'])
+async def send_tomorrow(msg: types.Message):
     dashboard = schedule.get_schedule_tomorrow()
-    await msg.reply(dashboard, parse_mode="HTML")
+    await msg.answer(dashboard, parse_mode="HTML")
 
 
-@dp.message_handler(commands=['след_неделя'])
-async def send_welcome(msg: types.Message):
+@dp.message_handler(commands=['next_week'])
+async def send_next_week(msg: types.Message):
     dashboard = schedule.get_schedule_next_week()
-    await msg.reply(dashboard, parse_mode="HTML")
+    await msg.answer(dashboard, parse_mode="HTML")
+
+@dp.message_handler(commands=['help'])
+async def send_help(msg: types.Message):
+    await msg.answer("""Команды:
+    /schedule - расписание на текущую неделю
+    /today - расписание на текущий день
+    /tomorrow - расписание на завтра
+    /next_week - расписание на следующую неделю
+    """)
 
 
 if __name__ == '__main__':
